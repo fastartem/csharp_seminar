9876043a851e845112b1d9bf0f62b44ca40bc7b8
Tomikus <tomikus1@gmail.com>
2023-09-11 12:48:55 +0300

    fix

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,12 +1,8 @@
 ﻿Console.Write("Enter number: ");
 int num = int.Parse(Console.ReadLine()??"0");
 
-for(int i = 2; i < num; i++){
+for(int i = 2; i <= num; i++){
     if(i % 2 == 0){
         Console.Write(i + ", ");
     }
-}
-
-if(num % 2 == 0){
-    Console.Write(num);
 }
