38199243aa175c2d7ddf14e129fe33b38ce44b1d
Tomikus <tomikus1@gmail.com>
2023-09-11 12:45:30 +0300

    code added

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine()??"0");
+
+for(int i = 2; i < num; i++){
+    if(i % 2 == 0){
+        Console.Write(i + ", ");
+    }
+}
+
+if(num % 2 == 0){
+    Console.Write(num);
+}
