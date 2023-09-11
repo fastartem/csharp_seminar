e3d2567676a61436c104ce5e2bc5d3f25391c806
Tomikus <tomikus1@gmail.com>
2023-09-11 12:09:54 +0300

    code added

--- Sem1Task4/Program.cs
+++ Sem1Task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter first number: ");
+int firstNum = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter second number: ");
+int secondNum = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter third number: ");
+int thirdNum = int.Parse(Console.ReadLine()??"0");
+
+int max = firstNum;
+if(secondNum > max){
+    max = secondNum;
+}
+if(thirdNum > max){
+    max = thirdNum;
+}
+
+Console.WriteLine("Max is " + max);
