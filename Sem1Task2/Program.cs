1b2915c81fcc0963452d968424a011bb7195bce1
Tomikus <tomikus1@gmail.com>
2023-09-11 12:01:04 +0300

    code added

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter first number: ");
+int firstNum = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter second number: ");
+int secondNum = int.Parse(Console.ReadLine()??"0");
+if (firstNum > secondNum){
+    Console.WriteLine("First number > second number");
+}
+else if (firstNum < secondNum){
+     Console.WriteLine("Second number > first number");
+}
+else {
+     Console.WriteLine("Numbers are equal");
+}
